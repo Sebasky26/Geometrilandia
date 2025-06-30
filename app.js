141a631732ca0c36d711861e44bc794e0b2340e2
Sebasky26 <sebasaisalla26@gmail.com>
2025-06-30 10:40:10 -0500

    Figuras Corregidas

--- app.js
+++ app.js
@@ -15,24 +15,24 @@ const PORT = process.env.PORT || 3000;
 
 // === Diccionario de UIDs RFID → Nombres de figuras ===
 const mapaRFID = {
-  "F6FE0885": "Estrella turquesa",
-  "B39DD90D": "Cuadrado azul",
-  "E1B7A07B": "Cuadrado rojo",
-  "55754239": "Estrella amarilla",
-  "4CA16D3B": "Corazon verde",
-  "F74B6E3B": "Cuadrado amarillo",
-  "BC124D39": "Estrella marron",
-  "22614239": "Circulo amarillo",
-  "8CAB6D3B": "Circulo turqueza",
-  "F7934D39": "Rectángulo azul",
-  "65EA4139": "Rectángulo verde",
-  "BB5F4239": "Corazon azul",
-  "E7BD4239": "Rectángulo turqueza",
-  "896A4D39": "Corazon rojo",
-  "AE9E4239": "Triangulo verde",
-  "B0DE6D3B": "Triangulo marron",
-  "91275D7B": "Triangulo rojo",
-  "C6770785": "Circulo marron"
+  "F6FE0885": "ESTRELLA TURQUESA",
+  "B39DD90D": "CUADRADO AZUL",
+  "E1B7A07B": "CUADRADO ROJO",
+  "55754239": "ESTRELLA AMARILLA",
+  "4CA16D3B": "CORAZON VERDE",
+  "F74B6E3B": "CUADRADO AMARILLO",
+  "BC124D39": "ESTRELLA MARRON",
+  "22614239": "CIRCULO AMARILLO",
+  "8CAB6D3B": "CIRCULO TURQUESA",
+  "F7934D39": "RECTÁNGULO AZUL",
+  "65EA4139": "RECTÁNGULO VERDE",
+  "BB5F4239": "CORAZON AZUL",
+  "E7BD4239": "RECTÁNGULO TURQUESA",
+  "896A4D39": "CORAZON ROJO",
+  "AE9E4239": "TRIANGULO VERDE",
+  "B0DE6D3B": "TRIANGULO MARRON",
+  "91275D7B": "TRIANGULO ROJO",
+  "C6770785": "CIRCULO MARRON"
 };
 
 // Middleware
